ae50a17c76e82beca56eab84bc75b50fceb6938a
Gabriel dos Santos <gabrields1910@gmail.com>
2021-01-12 15:37:36 -0300

    ex estrutura de dados

---
+++ 4-object-array/ex01-soma-intervalo.js
@@ -0,0 +1,9 @@
+function range(start, end){
+    var n = []
+    for(i = start; i <= end; i++){
+        n.push(i)
+    }
+    return n
+}
+
+console.log(range(-4,3))
--- tests.js
+++ tests.js
@@ -1,1 +1,12 @@
-//ÚLTIMA PARADA >>>>>  CAP.3 - ARGUMENTOS OPCIONAIS
+var day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running",
+             "television"]
+  };
+  console.log(day1.squirrel);
+  // → false
+  console.log(day1.wolf);
+  // → undefined
+  day1.wolf = false;
+  console.log(day1.wolf);
+  // → false
